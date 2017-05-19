85ab6e55719f5c7969f08845f7add1ee75f5618b
acidvegas <acidvegas@users.noreply.github.com>
2017-05-19 05:31:45 -0400

    Fixed greet.pl and added a delay setting.

--- .irssi/scripts/autorun/greet.pl
+++ .irssi/scripts/autorun/greet.pl
@@ -1,16 +1,29 @@
 use strict;
 use Irssi;
 use Irssi::Irc;
-sub send_greet {
-  my ($channel) = @_;
-  my $msg = Irssi::settings_get_str('greet_message');
-  $channel->command("/msg ".$channel->{name}." ".$msg);
 
+our $VERSION = '1.0';
+our %IRSSI = (
+    authors     => 'acidvegas (help from mr_vile/munki)',
+    contact     => 'acidvegas@supernets.org',
+    name        => 'Greet',
+    description => 'A script that sends a delayed message into the channel anytime you join.',
+    license     => 'ISC',
+    url         => 'http://github.com/acidvegas/irssi',
+);
+
+sub send_greet {
+    my ($channel) = @_;
+    my $msg = Irssi::settings_get_str('greet_message');
+    $channel->command("/msg ".$channel->{name}." ".$msg);
 }
 
 sub event_channel_sync {
-  my ($channel) = @_;
-  Irssi::timeout_add_once( 50000, &send_greet, $channel, );
+    my ($channel) = @_;
+    my $delay = Irssi::settings_get_int('greet_delay');
+    Irssi::timeout_add_once( $delay * 1000, \&send_greet, $channel, );
 }
+
+Irssi::settings_add_int('misc', 'greet_delay', 3);
 Irssi::settings_add_str('misc', 'greet_message', "<-");
 Irssi::signal_add_last('channel sync', 'event_channel_sync');
